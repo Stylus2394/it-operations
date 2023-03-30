bcb3593cde1bb0beefc1a7667a6b4ce2f33d6d52
Frank <f.niejenhuis@mzh.nl>
2023-03-30 10:57:38 +0200

    Met kleurtjes!111!!1

--- waar_is_de_kat_geweest.py
+++ waar_is_de_kat_geweest.py
@@ -1,7 +1,19 @@
 import random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 toegestane_doosnummers = list(range(1,6))
+aantal_pogingen = 0
 
 # Plaats de kat in een willekeurige doos
 kat_positie = random.randint(1, 5)
@@ -11,18 +23,25 @@ dozen = ["[]", "[]", "[]", "[]", "[]"]
 
 # Game loop
 while True:
+    print(bcolors.HEADER, "De kat zit in doos", kat_positie, bcolors.ENDC)
+
     #Vraag gebruiker om een doosnummer
     doos_nummer = int(input("Kies een doosnummer (1 t/m 5): "))
- 
+
+    
     if doos_nummer not in toegestane_doosnummers:
         #doos_nummer = int(input("2. Kies een doosnummer (1 t/m 5): "))
-        print("Vul een nummer in tussen 1-5")
+        print(bcolors.FAIL, "Vul een nummer in tussen 1-5", bcolors.ENDC)
         continue       
+
+    aantal_pogingen += 1
+
+    print(bcolors.OKBLUE, "Aantal pogingen:", aantal_pogingen, bcolors.ENDC)
 
     # Kijk of de kat in de gekozen doos zit
     if doos_nummer == kat_positie:
-        print("Gefeliciteerd! Je hebt de kat gevonden.")
-        #break
+        print(bcolors.OKGREEN, "Gefeliciteerd! Je hebt de kat gevonden.", bcolors.ENDC)
+        print(bcolors.OKCYAN, "Je had", aantal_pogingen , " pogingen nodig om de kat te vinden.", bcolors.ENDC)    
     
     # Verplaats de kat
     if kat_positie == 1:
