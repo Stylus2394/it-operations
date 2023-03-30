c6fd68030793fb6783f54f6099101e44ce889ce7
Frank <f.niejenhuis@mzh.nl>
2023-03-30 10:43:51 +0200

    Met input afvangen werkend.

--- waar_is_de_kat_geweest.py
+++ waar_is_de_kat_geweest.py
@@ -1,7 +1,7 @@
 import random
 
 
-toegestane_doosnummers = list(range(1,5))
+toegestane_doosnummers = list(range(1,6))
 
 # Plaats de kat in een willekeurige doos
 kat_positie = random.randint(1, 5)
@@ -13,7 +13,11 @@ dozen = ["[]", "[]", "[]", "[]", "[]"]
 while True:
     #Vraag gebruiker om een doosnummer
     doos_nummer = int(input("Kies een doosnummer (1 t/m 5): "))
-    #if doos_nummer not in range(1,5):
+ 
+    if doos_nummer not in toegestane_doosnummers:
+        #doos_nummer = int(input("2. Kies een doosnummer (1 t/m 5): "))
+        print("Vul een nummer in tussen 1-5")
+        continue       
 
     # Kijk of de kat in de gekozen doos zit
     if doos_nummer == kat_positie:
@@ -27,7 +31,7 @@ while True:
         kat_positie -= 1
     else:
         kat_positie += random.choice([-1, 1])
-    
+
     # Update het spelbord
     dozen = ["[]" if i != kat_positie - 1 else "[Kat]" for i in range(5)]
     print("  ".join(dozen))
